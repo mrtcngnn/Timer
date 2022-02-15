03ff53c76a716666c4b00019630b693a1af2305d
Mert Can Gönen <71656016+MertCanGonen@users.noreply.github.com>
2022-02-15 17:45:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function zamanlayiciActive() {
 //KRONOMETRE PART
 var k;
 
-function start(){
-    k = setInterval(time,1000);
+function start() {
+    k = setInterval(time, 1000);
 }
 
 function time() {
@@ -20,23 +20,23 @@ function time() {
     var m = parseInt(document.getElementById('minutesk').innerHTML);
     var h = parseInt(document.getElementById('hoursk').innerHTML);
     var d = parseInt(document.getElementById('daysk').innerHTML);
-    if ( s < 60 ){
+    if (s < 60) {
         s += 1;
         document.getElementById('secondsk').innerHTML = s;
     }
-    if ( s == 60 ){
+    if (s == 60) {
         m += 1;
         document.getElementById('minutesk').innerHTML = m;
         s %= 60;
         document.getElementById('secondsk').innerHTML = s;
     }
-    if ( m == 60 ) {
+    if (m == 60) {
         h += 1;
         document.getElementById('hoursk').innerHTML = h;
         m %= 60;
         document.getElementById('minutesk').innerHTML = m;
     }
-    if ( h == 24 ){
+    if (h == 24) {
         d += 1;
         document.getElementById('daysk').innerHTML = d;
         h %= 24;
@@ -68,8 +68,8 @@ function geri() {
 //KRONOMETRE PART IS FINISHED!!!
 //ZAMANLAYICI PART
 var z;
-var is,im,ih,id;
-var s,m,h,d;
+var is, im, ih, id;
+var s, m, h, d;
 var time;
 
 function geriZamanlayici() {
@@ -86,9 +86,9 @@ function startZamanlayici() {
     im = parseInt(document.getElementById('minutesi').value);
     ih = parseInt(document.getElementById('hoursi').value);
     id = parseInt(document.getElementById('daysi').value);
-    if ( is >= 60  || im >= 60 || ih >=24 ){
+    if (is >= 60 || im >= 60 || ih >= 24) {
         alert("You exceeded the time limits!!!");
-    }else {
+    } else {
         document.getElementById('secondsz').innerHTML = is;
         document.getElementById('minutesz').innerHTML = im;
         document.getElementById('hoursz').innerHTML = ih;
@@ -100,26 +100,33 @@ function startZamanlayici() {
         s = parseInt(document.getElementById('secondsz').innerHTML);
         m = parseInt(document.getElementById('minutesz').innerHTML);
         h = parseInt(document.getElementById('hoursz').innerHTML);
-        d = parseInt(document.getElementById('daysz').innerHTML);   
-        time = s + (m*60) + (h*60*60) + (d*24*60*60);
-        z = setInterval(timeZamanlayici,1000);
+        d = parseInt(document.getElementById('daysz').innerHTML);
+        time = s + (m * 60) + (h * 60 * 60) + (d * 24 * 60 * 60);
+        z = setInterval(timeZamanlayici, 1000);
     }
 }
 
 function timeZamanlayici() {
-    var tmp = time;
-    d = parseInt(tmp / 86400);
-    tmp %= 86400;
-    h = parseInt(tmp / 3600);
-    tmp %= 3600;
-    m = parseInt(tmp / 60);
-    tmp %= 60; 
-    s = tmp;
-    document.getElementById('secondsz').innerHTML = s;
-    document.getElementById('minutesz').innerHTML = m;
-    document.getElementById('hoursz').innerHTML = h;
-    document.getElementById('daysz').innerHTML = d;
-    time--;
+    if (time == -1) {
+        $('#zamanlayici').fadeOut('1000');
+        $('#message').fadeIn('3000');
+        $('#message').fadeOut('10000');
+        resetZamanlayici();
+    } else {
+        var tmp = time;
+        d = parseInt(tmp / 86400);
+        tmp %= 86400;
+        h = parseInt(tmp / 3600);
+        tmp %= 3600;
+        m = parseInt(tmp / 60);
+        tmp %= 60;
+        s = tmp;
+        document.getElementById('secondsz').innerHTML = s;
+        document.getElementById('minutesz').innerHTML = m;
+        document.getElementById('hoursz').innerHTML = h;
+        document.getElementById('daysz').innerHTML = d;
+        time--;
+    }
 }
 
 function stopZamanlayici() {
@@ -132,7 +139,7 @@ function resetZamanlayici() {
     $('#minutesi').fadeIn('100');
     $('#hoursi').fadeIn('100');
     $('#daysi').fadeIn('100');
-    document.getElementById('secondszi').value = "0";
+    document.getElementById('secondsi').value = "0";
     document.getElementById('minutesi').value = "0";
     document.getElementById('hoursi').value = "0";
     document.getElementById('daysi').value = "0";
